bc410face64f649eb161bddd3440e7562e6e7312
feather <paigetech@gmail.com>
2015-07-26 22:23:48 -0400

    creates user

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,4 +40,14 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #Including to test requests
+  config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
+
+

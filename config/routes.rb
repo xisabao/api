1e2e71ab55a7743234a8723d72a867aadcd75055
feather <paigetech@gmail.com>
2015-08-21 15:05:01 -0700

    changed routing to be /api

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ MarketPlaceApi::Application.routes.draw do
   mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
   # Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy] do
         resources :products, :only => [:create, :update, :destroy]

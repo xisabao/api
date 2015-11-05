cd3c034c4272081157d5090d82f04be0f7c1acb1
paigetech <paigetech@gmail.com>
2015-11-05 09:26:09 -0800

    some changes, I'm not sure what

--- automations/test.rb
+++ automations/test.rb
@@ -1,13 +1,13 @@
-require 'rubygems'
-require 'selenium-webdriver'
+require 'watir-webdriver'
 require 'headless'
+puts "driving now"
 
-puts 'Driving Now'
-
-
-Headless.ly do
-  driver = Selenium::WebDriver.for(:firefox)
-  driver.get 'http://google.com'
-  puts driver.title
-end
-driver.quit
+headless = Headless.new
+headless.start
+profile = Selenium::WebDriver::Firefox::Profile.new
+profile.proxy = Selenium::WebDriver::Proxy.new :http => 'my.proxy.com:8080', :ssl => 'my.proxy.com:8080'
+browser = Watir::Browser.new :firefox, :profile => profile
+b = Watir::Browser.start 'http://myevent.oregonlive.com/web/event.php'
+puts b.title
+b.close
+headless.destroy

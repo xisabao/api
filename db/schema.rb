2fdf3965481ddc3c49ad6bd5b0712433516de56e
xisabao <ubiquitoustrees@gmail.com>
2016-02-06 16:19:46 -0800

    Remove product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729231614) do
-
-  create_table "products", force: :cascade do |t|
-    t.string   "title",      default: ""
-    t.decimal  "price",      default: 0.0
-    t.boolean  "published",  default: false
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "products", ["user_id"], name: "index_products_on_user_id"
+ActiveRecord::Schema.define(version: 20150727131905) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- spec/factories/products.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :product do
-    title { FFaker::Product.product_name }
-    price { rand() * 100 }
-    published false
-    user
-  end
-
-end
